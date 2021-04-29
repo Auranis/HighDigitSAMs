c427d4c48d647b37fccc4dcec6d9e6b45fb9109e
Ian <72825026+LetMePickThat@users.noreply.github.com>
2021-04-29 10:58:58 +0200

    Update sensors.lua

--- Mods/tech/HighDigitSAMs/Database/Sensors/sensors.lua
+++ Mods/tech/HighDigitSAMs/Database/Sensors/sensors.lua
@@ -490,6 +490,43 @@ GRAVE_STONE =
 
 declare_sensor(GRAVE_STONE)
 
+-- S-400 SENSORS
+
+CHEESE_BOARD =
+        {
+			Name = "S-400 96L6E sr",
+			category = SENSOR_RADAR,
+            type = RADAR_AS,
+            scan_volume =
+            {
+                azimuth = {-180.0, 180.0},
+                elevation = {-15.0, 60.0}
+            },
+            max_measuring_distance = 300000.0,
+            detection_distance =
+            {
+                [HEMISPHERE_UPPER] =
+                {
+                    [ASPECT_HEAD_ON] = 300000.0,
+                    [ASPECT_TAIL_ON] = 300000.0
+                },
+                [HEMISPHERE_LOWER] =
+                {
+                    [ASPECT_HEAD_ON] = 300000.0,
+                    [ASPECT_TAIL_ON] = 300000.0
+                }
+            },
+            lock_on_distance_coeff = 0.85,
+			multiple_targets_tracking = true,
+            velocity_limits =
+            {
+                radial_velocity_min = 15.0,
+            },
+            scan_period = 1.0,
+        }
+
+declare_sensor(CHEESE_BOARD)
+
 -- SAMPT/SAMPT NG SENSOR
 
 ARABEL =
