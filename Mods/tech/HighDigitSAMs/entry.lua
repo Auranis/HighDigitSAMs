4a0eecb865d6f2ea789bc8f42c20063c2b6e6bca
Eddie Crouch <edward.m.crouch@gmail.com>
2023-11-04 15:10:37 -0400

    Fixed display name for masted Tomb Stone. Updated entry.lua

--- Mods/tech/HighDigitSAMs/entry.lua
+++ Mods/tech/HighDigitSAMs/entry.lua
@@ -5,7 +5,7 @@ displayName   = _("High Digit SAMs"),
 shortName	  =   "HighDigitSAMs",
 state		 	  = "installed",
 developerName	  = "E. 'Wiki' Crouch",
-version		 	  = "1.4.3",	
+version		 	  = "2.0.0",	
 info		  =    _("Adds various modern and historical air defense units to DCS world."),	 
 encyclopedia_path = current_mod_path .. '/Encyclopedia',
 })
